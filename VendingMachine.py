6603c39085bb9965d93dd95b2bc8df10c5dc7b58
gaurav3815 <57182968+gaurav3815@users.noreply.github.com>
2019-12-04 16:06:36 +0000

    Update VendingMachine.py

--- VendingMachine.py
+++ VendingMachine.py
@@ -55,3 +55,111 @@ while True:
                                 change = cash_input - a1
                                 print("Your Change : £",change)
                                 Vending_Machine = Vending_Machine - change
+        elif user_input == "a2":
+            if a2_stock == 0:
+                print("Apple Juice are out of Stock!")
+            else:
+                print("Apple Juice")
+                print("Payment :",a2)
+                payment_method = input("Card or Cash? \n =")
+                while payment_method not in payment_method_word:
+                    print("Couldn't Recognise the Payment Method, Try again")
+                    payment_method = input("Card or Cash? \n =")
+                else:
+                    if payment_method in card_payment:
+                        if Account_Balance >= a2:
+                            Account_Balance = Account_Balance - a2
+                            print("Payment Sucessful")
+                            print(Account_Balance)
+                            a2_stock = a2_stock - 1
+                            print(a2_stock)
+                        else:
+                            print("You Don't Have enough funds")
+                    else:
+                        cash_input = float(input("Money amount = £"))
+                        if cash_input == a2:
+                            Cash_Balance = Cash_Balance - a2
+                            print("Payment Sucessful")
+                            print(Cash_Balance)
+                        elif cash_input < a2:
+                            print("Not enough, Pay", a2 - cash_input,"more.")
+                        else:
+                            while cash_input > Vending_Machine:
+                                print("Not enough funds on Machine for change enter less")
+                                cash_input = float(input("Money amount = £"))
+                            else:
+                                change = cash_input - a2
+                                print("Your Change : £",change)
+                                Vending_Machine = Vending_Machine - change
+        elif user_input == "a3":
+            if a3_stock == 0:
+                print("Banana Milkshake are out of Stock!")
+            else:
+                print("Banana Milkshake")
+                print("Payment :", a3)
+                payment_method = input("Card or Cash? \n =")
+                while payment_method not in payment_method_word:
+                    print("Couldn't Recognise the Payment Method, Try again")
+                    payment_method = input("Card or Cash? \n =")
+                else:
+                    if payment_method in card_payment:
+                        if Account_Balance >= a3:
+                            Account_Balance = Account_Balance - a3
+                            print("Payment Sucessful")
+                            print(Account_Balance)
+                            a3_stock = a3_stock - 1
+                            print(a3_stock)
+                        else:
+                            print("You Don't Have enough funds")
+                    else:
+                        cash_input = float(input("Money amount = £"))
+                        if cash_input == a3:
+                            Cash_Balance = Cash_Balance - a3
+                            print("Payment Sucessful")
+                            print(Cash_Balance)
+                        elif cash_input < a3:
+                            print("Not enough, Pay", a3 - cash_input, "more.")
+                        else:
+                            while cash_input > Vending_Machine:
+                                print("Not enough funds on Machine for change enter less")
+                                cash_input = float(input("Money amount = £"))
+                            else:
+                                change = cash_input - a3
+                                print("Your Change : £", change)
+                                Vending_Machine = Vending_Machine - change
+        elif user_input == "a4":
+            if a4_stock == 0:
+                print("KitKat are out of Stock!")
+            else:
+                print("KitKat")
+                print("Payment :", a4)
+                payment_method = input("Card or Cash? \n =")
+                while payment_method not in payment_method_word:
+                    print("Couldn't Recognise the Payment Method, Try again")
+                    payment_method = input("Card or Cash? \n =")
+                else:
+                    if payment_method in card_payment:
+                        if Account_Balance >= a4:
+                            Account_Balance = Account_Balance - a4
+                            print("Payment Sucessful")
+                            print(Account_Balance)
+                            a4_stock = a4_stock - 1
+                            print(a4_stock)
+                        else:
+                            print("You Don't Have enough funds")
+                    else:
+                        cash_input = float(input("Money amount = £"))
+                        if cash_input == a4:
+                            Cash_Balance = Cash_Balance - a4
+                            print("Payment Sucessful")
+                            print(Cash_Balance)
+                        elif cash_input < a4:
+                            print("Not enough, Pay", a4 - cash_input, "more.")
+                        else:
+                            while cash_input > Vending_Machine:
+                                print("Not enough funds on Machine for change enter less")
+                                cash_input = float(input("Money amount = £"))
+                            else:
+                                change = cash_input - a4
+                                print("Your Change : £", change)
+                                Vending_Machine = Vending_Machine - change
